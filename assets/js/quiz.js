32905b7bae54ec2d3be3c0c9be6b3c39b71988d5
kumenger <44558777+kumenger@users.noreply.github.com>
2023-01-23 20:31:24 -0700

    score saved to local storage

---
+++ assets/js/quiz.js
@@ -0,0 +1,34 @@
+var javascriptQuiz = [
+  {
+    question: "JavaScript is a ___ -side programming language.",
+    choose: ["client", "server", "both", "node"],
+    answer: "both",
+  },
+  {
+    question:
+      "Which of the following will write the message “Hello DataFlair!” in an alert box?",
+    choose: [
+      "alertBox(“Hello DataFlair!”);",
+      "alert(Hello DataFlair!);",
+      "msgAlert(“Hello DataFlair!”);",
+      "alert(“Hello DataFlair!”);",
+    ],
+    answer: "alert(Hello DataFlair!);",
+  },
+  {
+    question: "choose",
+    choose: [
+      "alertBox(“Hello DataFlair!”);",
+      "alert(Hello DataFlair!);",
+      "msgAlert(“Hello DataFlair!”);",
+      "alert(“Hello DataFlair!”);",
+    ],
+    answer: "alert(Hello DataFlair!);",
+  },
+  {
+    question: "How do you find the minimum of x and y using JavaScript??",
+    choose: ["min(x,y);", "Math.min(x,y);", "Math.min(xy);", "min(xy);"],
+    answer: "Math.min(x,y);",
+  },
+];
+export { javascriptQuiz };
